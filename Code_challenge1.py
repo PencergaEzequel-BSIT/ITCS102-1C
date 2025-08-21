cbfbe1dbf7f3de5d9c4fa7c9e53ccec6e2299117
PencergaEzequel-BSIT <leuqezeagrecnep@gmail.com>
2025-08-21 11:50:57 +0800

    Update Code_challenge1.py

--- Code_challenge1.py
+++ Code_challenge1.py
@@ -1,3 +1,1 @@
-print("\t\t\t\t*\n\t\tNt*\t\t*\n\t\t*\t\t\t\t*\n\t*\tlt\t\t
-\t\t*\n*\tlt\t HELLO EZE\t\t\t\t*\n\t*\t\t\t\t\t\t*\n\t\t*
-\t\t\t\t*\n\t\t\t*\t\t*\n\t\t\t\t*")
+print("\t\t\t\t*\n\t\t\t*\t\t*\n\t\t*\t\t\t\t*\n\t*\t\t\t\t\t\t*\n*\t\t\t HELLO EZE\t\t\t\t*\n\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t*\n\t\t\t*\t\t*\n\t\t\t\t*")
