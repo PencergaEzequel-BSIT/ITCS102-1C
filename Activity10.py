cd81a8c7e5f64be20a04ae470c54f00bc974e665
PencergaEzequel-BSIT <leuqezeagrecnep@gmail.com>
2025-08-27 13:59:16 +0800

    Create Activity10.py

---
+++ Activity10.py
@@ -0,0 +1,11 @@
+print("Welcome")
+u = 'burger'
+pw = 'milktea123'
+
+un = input("Enter Username: ")
+pw2 = input("Enter Password: ")
+
+if u == un and pw == pw2:
+	 print("Access Granted!")
+else:
+	print("Access Denied!!!!")
