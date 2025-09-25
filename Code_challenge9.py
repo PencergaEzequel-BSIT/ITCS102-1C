811fa0b09d2cd748869cb760145b824f3c2b67d3
PencergaEzequel-BSIT <leuqezeagrecnep@gmail.com>
2025-09-25 18:34:56 +0800

    Create Code_challenge9.py

---
+++ Code_challenge9.py
@@ -0,0 +1,6 @@
+countdown = int(input("🚀 Enter a number to start the countdown: "))
+
+print("\n--- Countdown Initiated ---\n")
+for i in range(countdown, 0, -1):
+    print(f"⏳ {i}...")
+print("\n✨🔥💥 Liftoff! The rocket has launched! 💥🔥✨")
