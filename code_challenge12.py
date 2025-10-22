cb9cc4019a912c6198bc7f8691482917b539f663
PencergaEzequel-BSIT <leuqezeagrecnep@gmail.com>
2025-10-23 00:15:19 +0800

    Create code_challenge12.py

---
+++ code_challenge12.py
@@ -0,0 +1,26 @@
+for a in range(1,6):
+    for b in range(11,a,-1):
+        print(" ", end=" ")
+    for c in range(a,0,-1):
+        print(c, end=" ")
+    for d in range(2,a+1):
+        print(d, end=" ")
+    print()
+
+for e in range(1,10):
+    for f in range(11,e,-1):
+        print(" ", end=" ")
+    for g in range(e,0,-1):
+        print(g, end=" ")
+    for h in range(2,e+1):
+        print(h, end=" ")
+    print()
+
+for i in range(9,0,-1):
+    for j in range(11,i,-1):
+        print(" ", end=" ")
+    for k in range(i,0,-1):
+        print(k, end=" ")
+    for l in range(2,i+1):
+        print(l, end=" ")
+    print()
